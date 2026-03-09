9b812b82a10077c4e06dcd7f859a9e870b6bda73
mine0059 <onojefemuemine@gmail.com>
2026-03-09 15:33:43 +0100

    make: express to serve static files

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import { attachSocketServer } from './socket/server.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
 const httpServer = http.createServer(app);
 
 const PORT = Number(process.env.PORT || 8000);
@@ -11,7 +18,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 attachSocketServer(httpServer);
 
 httpServer.listen(PORT, HOST, () => {
-    const baseUrl = HOST === '0.0.0.0' ?  `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
     console.log(`Server is running on ${baseUrl}`);
     console.log(`Socket.io server is running on ${baseUrl.replace('http', 'socket')}/socket`);
 });
