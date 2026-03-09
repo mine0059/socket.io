abd7680bfa7bfb54b23a8ca25130f1a490829436
mine0059 <onojefemuemine@gmail.com>
2026-03-09 15:32:34 +0100

    fix: playData bug

--- src/socket/server.js
+++ src/socket/server.js
@@ -30,7 +30,7 @@ initGame();
 console.log(orbs);
 
 export function attachSocketServer(httpServer) {
-    const io = new Server(httpServer, { 
+    const io = new Server(httpServer, {
         cors: {
             origin: ['http://localhost:3030'],
             credentials: true,
@@ -59,39 +59,40 @@ export function attachSocketServer(httpServer) {
             const playerData = new PlayerData(playerName, settings);
             player = new Player(socket.id, playerConfig, playerData);
             players.push(player);
+            playersForUsers.push({ playerData });
             // make a playerData object - the data specific to this plater that everyone needs to know
             // a master player object to house both 
             ackCallback({
                 orbs,
-                indexInPlayers:playersForUsers.length-1
+                indexInPlayers: playersForUsers.length - 1
             }); // send the orbs array back as an ack function!
 
             // the client sent over a tock!
             socket.on('tock', (data) => {
                 // a tock has come in before the plater is set up
                 // this is because the client kept tocking after disconnect
-                if(!player.playerConfig){
+                if (!player.playerConfig) {
                     return;
                 }
 
-                speed = player.playerConfig.speed;
+                const speed = player.playerConfig.speed;
                 const xV = player.playerConfig.xVector = data.xVector;
                 const yV = player.playerConfig.yVector = data.yVector;
 
                 //if player can move in the x, move
-                if((player.playerData.locX > 5 && xV < 0) || (player.playerData.locX < settings.worldWidth) && (xV > 0)){
+                if ((player.playerData.locX > 5 && xV < 0) || (player.playerData.locX < settings.worldWidth) && (xV > 0)) {
                     player.playerData.locX += speed * xV;
                 }
 
                 //if player can move in the y, move
-                if((player.playerData.locY > 5 && yV > 0) || (player.playerData.locY < settings.worldHeight) && (yV < 0)){
+                if ((player.playerData.locY > 5 && yV > 0) || (player.playerData.locY < settings.worldHeight) && (yV < 0)) {
                     player.playerData.locY -= speed * yV;
                 }
 
                 // check for the tocking player to hit orbs
                 const capturedOrbI = checkForOrbCollisions(player.playerData, player.playerConfig, orbs, settings);
                 // function return null if not collision, an index if there is a collision
-                if(capturedOrbI !== null) { //index could be 0, so check !null
+                if (capturedOrbI !== null) { //index could be 0, so check !null
                     //remove the orb that needs to be replaced (at capturedOrbI)
                     //add a new Orb
                     orbs.splice(capturedOrbI, 1, new Orb(settings));
@@ -105,25 +106,25 @@ export function attachSocketServer(httpServer) {
                     //emit to all sockets playing the game, the orbSwitch event so it can update orbs... just the new orb
                     io.to('game').emit('orbSwitch', orbData);
                     //emit to all sockets playing the game, the updateLeaderBoard event because someone just scored
-                    io.to('game').emit('updateLeaderBoard',getLeaderBoard());
+                    io.to('game').emit('updateLeaderBoard', getLeaderBoard());
                 }
 
                 // player collisions of tocking  player
-                const absorbData = checkForPlayerCollisions(player.playerData, player.playerConfig, playersForUsers, socket.id);
+                const absorbData = checkForPlayerCollisions(player.playerData, player.playerConfig, players, playersForUsers, socket.id);
                 if (absorbData) {
-                    io.to('game').emit('playerAbsorbed',absorbData);
-                    io.to('game').emit('updateLeaderBoard',getLeaderBoard());
+                    io.to('game').emit('playerAbsorbed', absorbData);
+                    io.to('game').emit('updateLeaderBoard', getLeaderBoard());
                 }
             });
 
             socket.on('disconnect', (reason) => {
                 console.log(reason);
-                for(let i = 0; i < players.length; i++) {
+                for (let i = 0; i < players.length; i++) {
                     if (players[i].socketId === player.socketId) {
                         // these are the droids we're looking for
                         // splice the plater out of the player AND playersForUsers
                         players.splice(i, 1, {})
-                        playersForUsers.splice(i,1,{})
+                        playersForUsers.splice(i, 1, {})
                         break;
                     }
                 }
@@ -137,11 +138,11 @@ export function attachSocketServer(httpServer) {
 }
 
 function initGame() {
-        //loop defaultNumberOfOrbs times, and push a new Orb() onto our array
-        for (let i = 0; i < settings.defaultNumberOfOrbs; i++) {
-            orbs.push(new Orb(settings));
-        }
+    //loop defaultNumberOfOrbs times, and push a new Orb() onto our array
+    for (let i = 0; i < settings.defaultNumberOfOrbs; i++) {
+        orbs.push(new Orb(settings));
     }
+}
 
 function getLeaderBoard() {
     const leaderBoardArray = players.map(curPLayer => {
